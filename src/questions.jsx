a7ac0dafd649fa2a339c8155b7e6b99c663afa6a
Elijah <0321-3622@lspu.edu.ph>
2024-11-19 08:07:34 +0800

    elii

---
+++ src/questions.jsx
@@ -0,0 +1,135 @@
+export const questions = [
+    {
+        question_aid: 1,
+        question_question: "Q1 Mahal niya pa kaya ako?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            }
+        ],
+    },
+    {
+        question_aid: 2,
+        question_question: "Q2 Uuwi ba ako ng Makati this coming week end?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Pwede na rin",
+                isCorrect: false,
+            }
+        ],
+    },
+    {
+        question_aid: 3,
+        question_question: "Q3 May pag-asa kaya ako sakanya?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            }
+        ],
+    },
+    {
+        question_aid: 4,
+        question_question: "Q4 Gusto ko ng outside fun?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            }
+        ],
+    },
+    {
+        question_aid: 5,
+        question_question: "Q5 Tara walwal?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Pwede rin",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            }
+        ],
+    },
+    {
+        question_aid: 6,
+        question_question: "Q6 Gusto kaya nilang mag 5man grind?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            }
+        ],
+    },
+    
+];
