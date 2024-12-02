04addd9b2fc355af9b3d2f7a19baf55d6cc3208a
Elijah <0321-3622@lspu.edu.ph>
2024-12-01 21:07:33 -0800

    keneme

--- src/components/pages/backend/partials/Footer.jsx
+++ src/components/pages/backend/partials/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='text-center border-t border-line border-opacity-45'>
     <p className='mb-0 '>
-        Jollibee 2024 - Developed by Zhanne Malik
+        Quiz 2024 - Developed by Zhanne Malik
     </p>
     </footer>
   )
